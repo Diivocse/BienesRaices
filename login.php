b9e8a6c3bacbd8caae706be5310e0c1ce2e21dc7
Diego <diegoocampo.x@gmail.com>
2025-08-14 19:10:50 -0500

    Se agregó la validaciones al momento del logín, verificar si está o no está registrado

--- login.php
+++ login.php
@@ -1,21 +1,70 @@
 <?php
+
+use Dom\Mysql;
+
 require 'includes/funciones.php';
+require 'includes/config/database.php';
+
+$db = conectarDB();
+
+# Control de errores
+$errores = [];
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+    $email = mysqli_real_escape_string($db, filter_var($_POST['email'], FILTER_VALIDATE_EMAIL));
+    $password = mysqli_real_escape_string($db, $_POST['password']);
+
+    if (!$email) {
+        $errores[] = "Verifica el correo, no es valido o tiene errores";
+    }
+
+    if (!$password) {
+        $errores[] = "Verifica el correo, no es valido o tiene errores";
+    }
+
+    # Validar si existe un usuario
+    if (empty($errores)) {
+        $query = "SELECT * FROM usuarios WHERE email = '$email';";
+
+        $resultado = mysqli_query($db, $query);
+
+        if ($resultado->num_rows) {
+            # Revisar si el password es correcto
+            $usuario = mysqli_fetch_assoc($resultado);
+            $auth = password_verify($password, $usuario['password']);
+
+            $auth ? "Usuario encontrado" : $errores[] = "Contraseña incorrecta" ;
+        } else {
+            $errores[] = "El usuario no existe";
+        }
+    }
+}
 
 incluirTemplate('header');
 ?>
 
 <main class="contenedor seccion contenido-centrado">
     <h1>Ingresar</h1>
+
+    <?php
+    foreach ($errores as $error): ?>
+        <div class="alerta error">
+            <?php echo $error; ?>
+        </div>
+    <?php
+    endforeach; ?>
+
     <fieldset>
-        <form class="formulario">
+        <form method="POST" class="formulario">
             <legend>Ingresa tus datos</legend>
             <label for="email">E-Mail</label>
-            <input type="text" placeholder="Tu Email" id="email">
+            <input type="text" name="email" placeholder="Tu Email" id="email" required>
 
             <label for="password"></label>
-            <input type="password" placeholder="Tu password" id="password">
+            <input type="password" name="password" placeholder="Tu password" id="password" required>
     </fieldset>
-        <input type="submit" value="Iniciar sección" class="boton boton-verde">
+    <input type="submit" value="Iniciar sección" class="boton boton-verde">
     </form>
 </main>
 
