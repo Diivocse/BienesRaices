01089e1b1cc76bbd3e3174af3564f33cc6d20738
Diego <diegoocampo.x@gmail.com>
2025-10-20 16:41:27 -0500

    Configuración inicial de Composer para el proyecto

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'diego/bienesraices_inicio',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'b60513c69c166d2ab4ad246ecfa81091e64f9bbb',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'diego/bienesraices_inicio' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'b60513c69c166d2ab4ad246ecfa81091e64f9bbb',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
